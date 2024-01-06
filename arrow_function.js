1eb1f1b227f8d864fc7da5664345a5e6c38fde14
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-01-06 14:22:37 +0600

    arrow function

---
+++ arrow_function.js
@@ -0,0 +1,14 @@
+// function tesX()
+// {
+//     return "HELLO WORLD";
+// }
+// console.log(tesX());
+
+const test = () => "HELLO WORLD";
+// console.log(test());
+
+const test_2 = () => {
+    console.log('NAIM');
+    return 'Karim'
+}
+console.log(test_2());
